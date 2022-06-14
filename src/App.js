ea890f651ff901686069dec241c1e613e57cd813
RooT_Kim <bdd3452@gmail.com>
2022-06-15 02:10:11 +0900

    React component css inline

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import ButtonGroup from '@mui/material/ButtonGroup';
 
 //jsx -> react를 만드는 페이스북 팀에서 사용자 정의 태그를 만들때 return 후 tag를 명시할때 따옴표를 사용하지 않도록 js를 확장한 언어이다.
 function Header(props) {
+  const myStyle = {
+    borderBottom: '1px solid gray',
+    padding: '10px',
+    fontSize: '20px',
+  };
   return (
-    <header>
+    <header style={myStyle}>
       <h1>
         <a
           href='/'
